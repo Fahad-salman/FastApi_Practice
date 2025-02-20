548daab4bdd238c486ac562e5a6d36277882f160
Fahad Alsaigh <Fd.alsaigh@gmail.com>
2025-02-20 19:33:06 +0300

    Get product by Id - API

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, HTTPException
 import pandas as pd
 
-df = pd.read_json("products.json")
+# install the following packages 
+# # pip install fastapi uvicorn
 
+# To run our API : ( py -m uvicorn FAST_API_Example:app --reload ) or ( python3 -m uvicorn FAST_API_Example:app --reload ) 
+# here's the API url (http://127.0.0.1:8000/products/)
+
+df = pd.read_json("products.json")
 
 NO_DATA_FOUND = 404
 app = FastAPI()
@@ -12,3 +17,11 @@ app = FastAPI()
 @app.get("/products/")
 def get_products():
     return df.to_dict(orient="records")
+
+
+@app.get("/product/{product_id}")
+def get_product_by_id(product_id: int):
+    product = df[df["product_id"] == product_id] # Filter by product_id
+    if product.empty:
+        raise HTTPException(status_code=NO_DATA_FOUND, detail="Product not found")
+    return product.to_dict(orient="records")[0] # Return first matching product as dict
